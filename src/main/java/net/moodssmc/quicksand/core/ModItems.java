b192c62f72e68e8e3ff5f5bc9164f321b3cef4b1
Moodsss <h.danni005@gmail.com>
2022-01-20 06:44:30 +0000

    Repair bucket and increment version

--- src/main/java/net/moodssmc/quicksand/core/ModItems.java
+++ src/main/java/net/moodssmc/quicksand/core/ModItems.java
@@ -3,6 +3,7 @@ package net.moodssmc.quicksand.core;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SolidBucketItem;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,7 +15,12 @@ public class ModItems
     public static final DeferredRegister<Item> REGISTER = DeferredRegister.create(ForgeRegistries.ITEMS, Reference.MOD_ID);
 
     public static final RegistryObject<Item> QUICKSAND_BUCKET = REGISTER.register("quicksand_bucket",
-            () -> new SolidBucketItem(ModBlocks.QUICKSAND.get(), SoundEvents.SAND_PLACE, new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC)));
+            () -> new SolidBucketItem(ModBlocks.QUICKSAND.get(), SoundEvents.SAND_PLACE, new Item.Properties().setNoRepair().stacksTo(1).tab(CreativeModeTab.TAB_MISC)) {
+                @Override
+                public boolean isBookEnchantable(ItemStack stack, ItemStack book) {
+                    return false;
+                }
+            });
 
     private ModItems()
     {
